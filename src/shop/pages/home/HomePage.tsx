2d36be4f0bbddd7aea0d7bea4bb342ee48466ae1
Oscar Bazan <oscaribh97@gmail.com>
2026-04-07 18:24:22 -0600

    adding custom font from google fonts

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 export const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div>
+            <h1>Hola Mundo</h1>
+            <h1 className="font-monsterrat font-bold">Hola Mundo</h1>
+            <Button>Hola Mundo</Button>
+        </div>
     )
 }
