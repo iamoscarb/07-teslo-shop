9b859ea28feef9f1b55404b7fd51e819083a0c4c
Oscar Bazan <oscaribh97@gmail.com>
2026-04-08 14:23:07 -0600

    changing shopLayout and homePage

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
 
 export const HomePage = () => {
     return (
-        <div>
-            <h1>Hola Mundo</h1>
-            <h1 className="font-monsterrat font-bold">Hola Mundo</h1>
-            <Button>Hola Mundo</Button>
-        </div>
+        <>
+            <CustomJumbotron title="Todos los productos" />
+
+            <CustomPagination totalPages={7} />
+        </>
     )
 }
