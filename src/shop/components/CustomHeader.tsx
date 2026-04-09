74cbe1bd06793850fee680f40fa76de23860e204
Oscar Bazan <oscaribh97@gmail.com>
2026-04-09 17:00:48 -0600

    adding CustomLogo component to CustomFooter and CustomHeader components

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,36 +1,66 @@
-import { Search, ShoppingBag, Menu } from "lucide-react";
+import { Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
-
+import { useRef } from "react";
+import { Link, useParams, useSearchParams } from "react-router";
+import { cn } from "@/lib/utils";
+import { CustomLogo } from "@/components/custom/CustomLogo";
 
 export const CustomHeader = () => {
-    const [cartCount] = useState(3);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { gender } = useParams();
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const query = searchParams.get('query') || '';
+
+    const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key !== 'Enter') return;
+
+        const query = inputRef.current?.value;
+
+        const newSearchParams = new URLSearchParams();
+
+        if (!query) {
+            newSearchParams.delete('query')
+        } else {
+            newSearchParams.set('query', query);
+        }
+
+        setSearchParams(newSearchParams);
+    }
+
     return <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
         <div className="container mx-auto px-4 lg:px-8">
             <div className="flex h-16 items-center justify-between">
                 {/* Logo */}
-                <div className="flex items-center space-x-4">
-                    <Button variant="ghost" size="icon" className="md:hidden">
-                        <Menu className="h-5 w-5" />
-                    </Button>
-                    <h1 className="text-xl font-semibold tracking-tight">TESLA STYLE</h1>
-                </div>
+                <CustomLogo />
 
                 {/* Navigation - Desktop */}
                 <nav className="hidden md:flex items-center space-x-8">
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Camisetas
-                    </a>
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Sudaderas
-                    </a>
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Chaquetas
-                    </a>
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Accesorios
-                    </a>
+                    <Link to="/"
+                        className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                            !gender ? 'underline underline-offset-4 font-bold' : ''
+                        )}>
+                        Todo
+                    </Link>
+                    <Link to="/gender/men"
+                        className={cn('text-sm font-medium transition-colors hover:text-primary',
+                            gender === 'men' ? 'underline underline-offset-4 font-bold' : ''
+                        )}>
+                        Hombres
+                    </Link>
+                    <Link to="/gender/women"
+                        className={cn('text-sm font-medium transition-colors hover:text-primary',
+                            gender === 'women' ? 'underline underline-offset-4 font-bold' : ''
+                        )}>
+                        Mujeres
+                    </Link>
+                    <Link to="/gender/kid"
+                        className={cn('text-sm font-medium transition-colors hover:text-primary',
+                            gender === 'kid' ? 'underline underline-offset-4 font-bold' : ''
+                        )}>
+                        Niños
+                    </Link>
                 </nav>
 
                 {/* Search and Cart */}
@@ -38,20 +68,26 @@ export const CustomHeader = () => {
                     <div className="hidden md:flex items-center space-x-2">
                         <div className="relative">
                             <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-                            <Input placeholder="Buscar productos..." className="pl-9 w-64 h-9" />
+                            <Input ref={inputRef} placeholder="Buscar productos..." className="pl-9 w-64 h-9 bg-white"
+                                onKeyDown={handleSearch} defaultValue={query} />
                         </div>
                     </div>
 
                     <Button variant="ghost" size="icon" className="md:hidden">
                         <Search className="h-5 w-5" />
                     </Button>
+                    <Link to='/auth/login'>
+                        <Button variant='default' size='sm' className="ml-2">
+                            Login
+                        </Button>
+                    </Link>
 
-                    <Button variant="ghost" size="icon" className="relative">
-                        <ShoppingBag className="h-5 w-5" />
-                        {cartCount > 0 && <span className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-primary-foreground text-xs flex items-center justify-center">
-                            {cartCount}
-                        </span>}
-                    </Button>
+                    <Link to='/admin'>
+                        <Button variant='destructive' size='sm' className="ml-2">
+                            Admin
+                        </Button>
+                    </Link>
+
                 </div>
             </div>
         </div>
