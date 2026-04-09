9f13f2d0684c88f9a12f01add197823b0f557312
Oscar Bazan <oscaribh97@gmail.com>
2026-04-09 17:01:28 -0600

    change in GenderPage

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,23 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import type { _ } from "node_modules/react-router/dist/development/context-phCt_zmH.d.mts"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+    const { gender } = useParams();
+    const genderLabel = gender === 'men' ? 'Hombres' :
+        gender === 'women' ? 'Mujeres' :
+            'Niños';
+
     return (
-        <div>GenderPage</div>
+        <>
+            <CustomJumbotron title={`Productos para ${genderLabel}`} />
+            <ProductsGrid products={products} />
+
+            <CustomPagination totalPages={7} />
+        </>
     )
 }
