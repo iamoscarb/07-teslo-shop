ecd5265fa98a165dd5481e898a5c39cb565f35b5
Oscar Bazan <oscaribh97@gmail.com>
2026-04-13 14:36:40 -0600

    change in AdminProductsPage

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,5 +1,49 @@
+import { AdminTitle } from "@/admin/components/AdminTitle"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { Button } from "@/components/ui/button"
+import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "@/components/ui/table"
+import { PlusIcon } from "lucide-react"
+import { Link } from "react-router"
+
 export const AdminProductsPage = () => {
     return (
-        <div>AdminProductsPage</div>
+        <>
+
+            <div className="flex justify-between items-center">
+                <AdminTitle title="Productos" subtitle="Gestión de productos" />
+
+                <Link to="/admin/products/new">
+                    <Button>
+                        <PlusIcon />
+                        Nuevo Producto
+                    </Button></Link>
+            </div>
+
+
+            <Table className="bg-white p-10 shadow-sx border border-gray-200 mb-10">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">ID</TableHead>
+                        <TableHead>Imagen</TableHead>
+                        <TableHead>Nombre</TableHead>
+                        <TableHead>Precio</TableHead>
+                        <TableHead>Categoría</TableHead>
+                        <TableHead>Inventario</TableHead>
+                        <TableHead>Tallas</TableHead>
+                        <TableHead className="text-right">Acciones</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+
+            <CustomPagination totalPages={5} />
+        </>
     )
 }
