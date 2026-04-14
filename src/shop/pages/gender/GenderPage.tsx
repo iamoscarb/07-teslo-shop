64ca3501a23af98f56b2571fb47d9986f8fced92
Oscar Bazan <oscaribh97@gmail.com>
2026-04-14 17:28:17 -0600

    calling on api in homepage and genderpage pages

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,13 +1,14 @@
 import { CustomPagination } from "@/components/custom/CustomPagination"
-import { products } from "@/mocks/products.mock"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
 import { ProductsGrid } from "@/shop/components/ProductsGrid"
-import type { _ } from "node_modules/react-router/dist/development/context-phCt_zmH.d.mts"
+import { useProducts } from "@/shop/hooks/useProducts"
 import { useParams } from "react-router"
 
 export const GenderPage = () => {
 
     const { gender } = useParams();
+    const { data } = useProducts();
+
     const genderLabel = gender === 'men' ? 'Hombres' :
         gender === 'women' ? 'Mujeres' :
             'Niños';
@@ -15,7 +16,7 @@ export const GenderPage = () => {
     return (
         <>
             <CustomJumbotron title={`Productos para ${genderLabel}`} />
-            <ProductsGrid products={products} />
+            <ProductsGrid products={data?.products || []} />
 
             <CustomPagination totalPages={7} />
         </>
